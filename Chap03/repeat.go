60ae49d3c9354652585afa940668a1baea12aa34
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2024-07-20 17:30:04 -0300

    Alteração da função para definir que caracter deve ser impresso

--- Chap03/repeat.go
+++ Chap03/repeat.go
@@ -1,5 +1,12 @@
 package repeat
 
-func ImprimirCaracter() string{
-	return "*****"
+func ImprimirCaracter(caracter string) string{
+	
+	var saida string
+	
+	for i := 0 ; i < 5 ; i++ {
+		saida = saida + caracter
+	}
+	
+	return saida
 }
--- Chap03/repeat_test.go
+++ Chap03/repeat_test.go
@@ -3,10 +3,19 @@ package repeat
 import "testing"
 
 func TestImprimirCaracter(t *testing.T) {
-	resultado := ImprimirCaracter()
+	resultado := ImprimirCaracter("*")
 	esperado := "*****"
 	
 	if resultado != esperado {
 		t.Errorf("Resultado '%s', Esperado '%s'", resultado, esperado)
 	}
+}
+
+func TestImprimirCaracterInformado(t *testing.T) {
+	resultado := ImprimirCaracter("a")
+	esperado := "aaaaa"
+	
+	if resultado != esperado {
+		t.Errorf("Resultado '%s', Esperado '%s'", resultado, esperado)
+	} 
 }
