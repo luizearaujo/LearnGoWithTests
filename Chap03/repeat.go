9ff91891bb5a6f261f67648a9c9e0441fe3a5de5
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2024-07-20 17:31:59 -0300

    Refatoração para definir constante da quantidade de repetições e utilização do operador +=

--- Chap03/repeat.go
+++ Chap03/repeat.go
@@ -1,11 +1,13 @@
 package repeat
 
+const quantidadeRepeticao = 5
+
 func ImprimirCaracter(caracter string) string{
 	
 	var saida string
 	
-	for i := 0 ; i < 5 ; i++ {
-		saida = saida + caracter
+	for i := 0 ; i < quantidadeRepeticao ; i++ {
+		saida += caracter
 	}
 	
 	return saida
