6951793d2aac25aa1ab07df6d3377ccfa0c66e09
Luiz Eduardo de Araujo <luiz.earaujo@gmail.com>
2024-07-24 16:27:10 -0300

    Implementação da função SomaTudo, que recebe uma quantidade variável de slices e retorna um slice onde cada elemento é o somatório dos elementos dos slices passados como parâmentro para a função

--- Chap04_Array_e_Slices/soma.go
+++ Chap04_Array_e_Slices/soma.go
@@ -1,10 +1,21 @@
 package soma
 
 func Soma(lista []int) int {
-	
+
 	resultado := 0
-	for _,numero := range lista {
+	for _, numero := range lista {
 		resultado += numero
 	}
 	return resultado
+}
+
+func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
+	quantidadeDeNumeros := len(numerosParaSomar)
+	somas = make([]int, quantidadeDeNumeros)
+
+	for i, numeros := range numerosParaSomar {
+		somas[i] = Soma(numeros)
+	}
+
+	return
 }
--- Chap04_Array_e_Slices/soma_test.go
+++ Chap04_Array_e_Slices/soma_test.go
@@ -1,28 +1,42 @@
 package soma
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSomaComArray(t *testing.T) {
-	
-	numeros := []int{1,2,3}
+
+	numeros := []int{1, 2, 3}
 	resultado := Soma(numeros)
 	esperado := 6
-	
+
 	if resultado != esperado {
-		t.Errorf("Resultado '%d', Esperado '%d', Dado '%v'", resultado, 
+		t.Errorf("Resultado '%d', Esperado '%d', Dado '%v'", resultado,
 			esperado, numeros)
 	}
-	
+
 }
 
 func TestaSomaComSlice(t *testing.T) {
-	
-	lista := []int{1,2,3,4}
+
+	lista := []int{1, 2, 3, 4}
 	resultado := Soma(lista)
 	esperado := 10
-	
+
 	if resultado != esperado {
 		t.Errorf("Resultado '%d', Esperado '%d', Dados '%v'", resultado,
 			esperado, lista)
 	}
+}
+
+func TestSomaTudo(t *testing.T) {
+
+	resultado := SomaTudo([]int{1, 2}, []int{0, 9})
+	esperado := []int{3, 9}
+
+	if !reflect.DeepEqual(resultado, esperado) {
+		t.Errorf("Resultado '%v', Esperado '%v'", resultado, esperado)
+	}
+
 }
