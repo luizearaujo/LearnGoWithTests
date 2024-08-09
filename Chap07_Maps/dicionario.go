03f21213db72538d9a32c2b792726ec249ca1762
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2024-08-09 16:54:06 -0300

    Validação da existencia do index na busca. Retorno de erro caso o index não exista.

--- Chap07_Maps/dicionario.go
+++ Chap07_Maps/dicionario.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	
+	"errors"
 )
+
+var ErrorIndexNotFound = errors.New("Não foi possível encontrar a informação a partir do index.")
 
 type Dicionario map[string]string
 
-func (d Dicionario) Busca(index string) string {
-	return d[index]
+func (d Dicionario) Busca(index string) (string, error) {
+	definicao, existe := d[index]
+	if !existe {
+		return "", ErrorIndexNotFound
+	}
+	return definicao, nil
 }
--- Chap07_Maps/dicionario_test.go
+++ Chap07_Maps/dicionario_test.go
@@ -7,11 +7,20 @@ import (
 func TestBusca(t *testing.T) {
 	
 	dicionario := Dicionario{"teste":"isso é apenas um teste"}
+		
+	t.Run("chave existente", func(t *testing.T) {
+		actual, _ := dicionario.Busca("teste")
+		expected := "isso é apenas um teste"
 	
-	actual := dicionario.Busca("teste")
-	expected := "isso é apenas um teste"
+		comparaString(t, actual, expected)	
+	})
 	
-	comparaString(t, actual, expected)
+	t.Run("chave inexistente", func(t *testing.T) {
+		_, err := dicionario.Busca("desconhecido")
+		
+		comparaString(t, err.Error(), ErrorIndexNotFound.Error())
+	})
+	
 }
 
 func comparaString(t *testing.T, actual, expected string) {
